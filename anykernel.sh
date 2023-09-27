4466033a47e2a3dc61547156cb8d79e3656718ba
Blue Green <45290812+BlueGreen82@users.noreply.github.com>
2023-09-28 06:17:37 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -3,14 +3,14 @@
 
 # Set properties
 properties() { '
-kernel.string=UndergroundKernel for Xiaomi Redmi Note 4(X)
+kernel.string=KopiCetheProjects for Xiaomi Redmi Note 4(X)
 do.devicecheck=1
 do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=1
 device.name1=mido
-supported.versions=8.1.0 - 11
+supported.versions=8.1.0 - 10
 supported.patchlevels=
 '; }
 
